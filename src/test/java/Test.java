3e67a43d9c0cd0a422c73c31483556c7fed601ca
BT\ovidiu.nica <ovidiu.nica@btrl.ro>
2024-10-29 13:58:03 +0200

    first push

---
+++ src/test/java/Test.java
@@ -0,0 +1,12 @@
+public class Test extends BaseTest{
+
+//    @org.junit.Test
+//    public void testOpenPage() {
+//        String actualTitle = driver.getTitle();
+//    }
+
+    @org.testng.annotations.Test
+    public void testOpenPage2() {
+        String actualTitle = driver.getTitle();
+    }
+}
